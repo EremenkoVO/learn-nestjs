b4d9d12a03350bdb365032af39841edf1a5f0a95
EremenkoVO <eremenko.v.o@yandex.ru>
2021-12-07 01:21:51 +0300

    add review controller

---
+++ src/review/review.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ReviewModel } from './review.model';
+
+@Controller('review')
+export class ReviewController {
+  /**
+   * Создание ревью
+   * @param dto модель ревью
+   */
+  @Post('create')
+  async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+
+  /**
+   * Удаление ревью
+   * @param id id ревью
+   */
+  @Delete(':id')
+  async delete(@Param('id') id: string) {}
+
+  @Get('byProduct/:productId')
+  async getByProduct(@Param('productId') productId: string) {}
+}
