4b8ba6ede6821528d9c25d3b127fc84321760857
EremenkoVO <eremenko.v.o@yandex.ru>
2021-12-09 18:12:34 +0300

    add mongoose

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,4 +1,12 @@
-export class AuthModel {
+import { prop } from '@typegoose/typegoose';
+import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+export class AuthModel extends TimeStamps {
+  _id: string;
+
+  @prop()
   email: string;
+
+  @prop()
   passwordHash: string;
 }
